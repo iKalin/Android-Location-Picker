708c52b57505ce40b06af0ce0221cdc464499dd8
Bibol Bolat <bolat.bibol@gmail.com>
2015-09-09 18:28:53 +0600

    delete currentlocation request code

--- app/src/main/java/com/example/ultron/androidlocationpicker/LocationPickerActivity.java
+++ app/src/main/java/com/example/ultron/androidlocationpicker/LocationPickerActivity.java
@@ -1,17 +1,8 @@
 package com.example.ultron.androidlocationpicker;
 
-import android.location.Location;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationAvailability;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,15 +12,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import static com.google.android.gms.common.api.GoogleApiClient.*;
-
 public class LocationPickerActivity extends AppCompatActivity implements GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
 
     private MapView mMapView;
 
     private GoogleMap mGoogleMap;
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
 
     @Nullable
     private Marker mMarker = null;
@@ -61,76 +48,8 @@ public class LocationPickerActivity extends AppCompatActivity implements GoogleM
         mGoogleMap.setOnMapClickListener(this);
         mGoogleMap.setOnMapLongClickListener(this);
 
-
-        buildGoogleApiClient();
-        createLocationRequest();
     }
 
-    private void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
-
-    private synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new Builder(this)
-                .addConnectionCallbacks(new ConnectionCallbacks() {
-                    @Override
-                    public void onConnected(Bundle bundle) {
-
-                        Toast.makeText(LocationPickerActivity.this, "Success", Toast.LENGTH_SHORT).show();
-
-                        startLocationUpdates();
-
-                    }
-
-                    @Override
-                    public void onConnectionSuspended(int i) {
-
-                    }
-                })
-                .addOnConnectionFailedListener(new OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(ConnectionResult connectionResult) {
-                        Toast.makeText(LocationPickerActivity.this, "FAIL ;(", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-    private void startLocationUpdates() {
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, new com.google.android.gms.location.LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-
-
-                Toast.makeText(LocationPickerActivity.this, "dddddd", Toast.LENGTH_SHORT).show();
-                stopLocationUpdates();
-            }
-        });
-    }
-
-    protected void stopLocationUpdates() {
-        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult result) {
-                super.onLocationResult(result);
-
-                LatLng currentLocation = new LatLng(result.getLastLocation().getLatitude(), result.getLastLocation().getLongitude());
-
-                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLocation, 12);
-                mGoogleMap.animateCamera(cameraUpdate);
-
-            }
-
-            @Override
-            public void onLocationAvailability(LocationAvailability locationAvailability) {
-                super.onLocationAvailability(locationAvailability);
-            }
-        });
-    }
 
     @Override
     public void onMapClick(LatLng latLng) {
@@ -162,7 +81,6 @@ public class LocationPickerActivity extends AppCompatActivity implements GoogleM
     public void onDestroy() {
         super.onDestroy();
         mMapView.onDestroy();
-        stopLocationUpdates();
     }
 
     @Override
